9646c9e3c84d86ad7df1963d336f781b3b0d5cc0
Pravin Pandey <pravinpandey.903@gmail.com>
2022-08-30 18:10:39 +0530

    Initial Code

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import millify from 'millify';
+import { Collapse, Row, Col, Typography, Avatar, Result, Button } from 'antd';
+import HTMLReactParser from 'html-react-parser';
+
+import { useGetExchangesQuery } from '../services/cryptoApi';
+import Loader from './Loader';
+import Homepage from './Homepage';
+
+const { Text } = Typography;
+const { Panel } = Collapse;
+
+const Exchanges = () => {
+  const navigate = useNavigate();
+  const { data, isFetching } = useGetExchangesQuery();
+  const exchangesList = data?.data?.exchanges;
+  console.log(exchangesList);
+
+  const navigateHome = () =>{
+    navigate('/');
+  };
+
+  if(isFetching) return <Loader />;
+
+
+  return (
+    <Result
+          status="404"
+          title="404"
+          subTitle="Sorry, the page you visited does not exist."
+          extra={<Button type="primary" onClick={navigateHome}>Back Home</Button>}
+      />
+    // <>
+        
+    //   {/* <Row>
+    //     <Col span={6}>Exchanges</Col>
+    //     <Col span={6}>24h Trade Volume</Col>
+    //     <Col span={6}>Markets</Col>
+    //     <Col span={6}>Change</Col>
+    //   </Row>
+    //   <Row>
+    //     {/* {exchangesList.map((exchange) => (
+    //       <Col span={24}>
+    //         <Collapse>
+    //           <Panel
+    //             key={exchange.uuid}
+    //             showArrow={false}
+    //             header={(
+    //               <Row key={exchange.uuid}>
+    //                 <Col span={6}>
+    //                   <Text><strong>{exchange.rank}.</strong></Text>
+    //                   <Avatar className="exchange-image" src={exchange.iconUrl} />
+    //                   <Text><strong>{exchange.name}</strong></Text>
+    //                 </Col>
+    //                 <Col span={6}>${millify(exchange.volume)}</Col>
+    //                 <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+    //                 <Col span={6}>{millify(exchange.marketShare)}%</Col>
+    //               </Row>
+    //               )}
+    //           >
+    //             {HTMLReactParser(exchange.description || '')}
+    //           </Panel>
+    //         </Collapse>
+    //       </Col>
+    //     ))} */}
+    //   </Row> */}
+    // </>
+
+  )
+}
+
+export default Exchanges
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': 'ea846b206fmshd467576133ca66dp1a355bjsne348911c5263'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query:(count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getExchanges: builder.query({
+            query:() => createRequest(`/exchanges`)
+        }),
+        getCryptoDetails: builder.query({
+            query:(coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query:({coinId, timePeriod}) => createRequest(`coin/${coinId}/history?timePeriod=${timePeriod}`),
+        })
+    })
+});
+
+
+export const {useGetCryptosQuery, useGetExchangesQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery} = cryptoApi;
+
